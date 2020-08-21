1e06469bb54a37385fc9ba3c95f33c78b793a33f
fajjarnr <zfajart@gmail.com>
2020-08-21 10:35:41 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bbe46820020ae3af95d0defd0a523169'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e3fb5d37-5182-456f-96ee-6f7f942f1432'
+};
